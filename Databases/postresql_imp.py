40c78a01b90cf44951b48c667717cdd83792f290
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-04-25 20:14:06 +0545

    creation of database engine

---
+++ Databases/postresql_imp.py
@@ -0,0 +1,21 @@
+from sqlmodel import Field, SQLModel, create_engine
+
+
+class Review(SQLModel, table=True):
+    id: int | None = Field(default=True, primary_key=True, gt=0)
+    text: str
+    label: str | None = None
+    source_api: str
+
+
+db_url = "postgresql+psycopg://dev_user:dev_password@localhost:5432/review_db"
+engine = create_engine(db_url)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    create_db_and_tables()
+    print("Database is ready for reviews!")
