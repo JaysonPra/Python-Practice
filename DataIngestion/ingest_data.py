fe06bdba9facb8ea5e849b38224a962a51f66da1
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-05-04 20:22:01 +0545

    removed session context manager from the function and rely on dependency injection

--- DataIngestion/ingest_data.py
+++ DataIngestion/ingest_data.py
@@ -1,6 +1,5 @@
 from typing import List
 
-from database import engine
 from logging_config import setup_logger
 from loguru import logger
 from models import Review
@@ -9,28 +8,18 @@ from sqlmodel import Session
 setup_logger()
 
 
-def add_review(reviews: List[dict]) -> None:
-    with Session(engine) as session:
-        for i, review_dict in enumerate(reviews):
-            try:
-                validated_review = Review.model_validate(review_dict)
-                session.add(validated_review)
-                logger.success(f"Review {i} added successfully!")
-            except Exception as e:
-                logger.critical(f"Failed to add review {i}: {e}")
-
+def add_review(reviews: List[dict], session: Session) -> None:
+    for i, review_dict in enumerate(reviews):
         try:
-            session.commit()
-            logger.success("Transaction committed successfully!")
+            validated_review = Review.model_validate(review_dict)
+            session.add(validated_review)
+            logger.success(f"Review {i} added successfully!")
         except Exception as e:
-            session.rollback()
-            logger.critical(f"Database commit failed: {e}")
+            logger.critical(f"Failed to add review {i}: {e}")
 
-
-raw_data = [
-    {"text": "Amazing review!", "sentiment": "positive", "score": 0.95},
-    {"text": "Terrible...", "sentiment": "negative"},
-    {"text": 12345, "sentiment": "neutral", "score": "high"},
-    {"text": "Okay", "sentiment": "neutral", "score": 0.5},
-]
-add_review(raw_data)
+    try:
+        session.commit()
+        logger.success("Transaction committed successfully!")
+    except Exception as e:
+        session.rollback()
+        logger.critical(f"Database commit failed: {e}")
