b0ab377addb8d55776ebc6197daf1a86e8ca4bed
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-05-09 20:09:15 +0545

    descriptor prototype for model hyperparameter configuration

---
+++ Dunder/descriptor.py
@@ -0,0 +1,32 @@
+from typing import Any, Callable, Generic, TypeVar
+
+T = TypeVar("T", int, float)
+
+
+class Verified(Generic[T]):
+    def __init__(self, validator: Callable[[T], bool]) -> None:
+        self.validator = validator
+        self.name = ""
+
+    def __set_name__(self, owner: Any, name: str):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance.__dict__.get(self.name)
+
+    def __set__(self, instance, value):
+        if not self.validator(value):
+            raise ValueError(f"Invalid value for {self.name}: {value}")
+        instance.__dict__[self.name] = value
+
+
+class ModelConfig:
+    lr = Verified(lambda x: 0 < x < 1)
+    batch_size = Verified(lambda x: x > 0)
+
+
+config = ModelConfig()
+config.lr = 0.5
+print(config.lr)
