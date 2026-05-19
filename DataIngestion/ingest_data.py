a9f04c1a7d79196e7264e7696b9ce13e8d7fe9d8
Jayson Pradhananga <jaysonyt2@gmail.com>
2026-05-19 19:40:53 +0545

    corrected exceptions and logger levels

--- DataIngestion/ingest_data.py
+++ DataIngestion/ingest_data.py
@@ -1,23 +1,35 @@
 from logging_config import setup_logger
 from loguru import logger
 from models import Review
+from pydantic import ValidationError
+from sqlalchemy.exc import SQLAlchemyError
 from sqlmodel import Session
 
 setup_logger()
 
 
 def add_review(reviews: list[dict], session: Session) -> None:
+    valid_review_count = 0
+
     for i, review_dict in enumerate(reviews):
         try:
             validated_review = Review.model_validate(review_dict)
             session.add(validated_review)
-            logger.success(f"Review {i} added successfully!")
-        except Exception as e:
-            logger.critical(f"Failed to add review {i}: {e}")
+            valid_review_count += 1
+
+        except ValidationError:
+            logger.exception(f"Skipping index {i} due to invalid schema.")
+
+    if valid_review_count == 0:
+        logger.info("No valid reviews to commit")
+        return
 
     try:
         session.commit()
-        logger.success("Transaction committed successfully!")
-    except Exception as e:
+        logger.success(
+            f"Succesfully committed {valid_review_count} reviews to the database!"
+        )
+
+    except SQLAlchemyError:
         session.rollback()
-        logger.critical(f"Database commit failed: {e}")
+        logger.exception("Database transaction failed to commit. Rolled back.")
