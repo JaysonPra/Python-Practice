f303dc6e27ca6968fc5b8942648b206d8296b167
Jayson Pradhananga <74088170+JaysonPra@users.noreply.github.com>
2026-05-26 20:15:47 +0545

    using google_play_scraper to imitate a live API

---
+++ Prefect/ingestion.py
@@ -0,0 +1,39 @@
+import datetime
+import uuid
+from typing import Annotated, Any
+
+from google_play_scraper import Sort, reviews
+from pydantic import BaseModel, BeforeValidator, Field
+from rich import print
+
+
+def cast_to_uuid(v: str | uuid.UUID) -> uuid.UUID:
+    if isinstance(v, str):
+        return uuid.UUID(v)
+    return v
+
+
+UUID_TYPE = Annotated[uuid.UUID, BeforeValidator(cast_to_uuid)]
+
+
+class Review(BaseModel):
+    reviewId: UUID_TYPE
+    at: datetime.datetime
+
+    content: str = Field(min_length=1)
+    score: int = Field(ge=1, le=5)
+    thumbsUpCount: int
+
+
+payload: tuple[list[dict[str, Any]]] = reviews(
+    "com.JindoBlu.OfflineGames",
+    lang="en",
+    country="us",
+    sort=Sort.NEWEST,
+    count=1,
+)
+
+review, continuation_token = payload
+
+review_model = Review.model_validate(review[0])
+print(review_model.reviewId, type(review_model.reviewId))
