dd7f4b0cfda12dd3a7ae37e1a0019ebbd248c52b
Jayson Pradhananga <74088170+JaysonPra@users.noreply.github.com>
2026-05-29 20:12:14 +0545

    basic hypothesis testing

---
+++ Hypothesis/mock.py
@@ -0,0 +1,12 @@
+from hypothesis import given
+from hypothesis import strategies as st
+
+
+def reverse(input_str: str) -> str:
+    if isinstance(input_str, str):
+        return input_str[::-1]
+
+
+@given(st.text())
+def test_reverse(input_str: str) -> None:
+    assert (reverse(reverse(input_str))) == input_str
